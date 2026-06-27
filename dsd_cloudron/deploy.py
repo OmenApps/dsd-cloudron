b829906f1dae80bd5ada3dcdd05a81105ca8acc5
Jack Linke <jacklinke@gmail.com>
2026-06-27 18:55:52 -0400

    Wire the four Cloudron pluggy hooks

--- dsd_cloudron/deploy.py
+++ dsd_cloudron/deploy.py
@@ -2,7 +2,9 @@
 
 import django_simple_deploy
 
+from .platform_deployer import PlatformDeployer
 from .plugin_config import plugin_config
+from .cli import PluginCLI, validate_cli
 
 
 @django_simple_deploy.hookimpl
@@ -12,8 +14,18 @@ def dsd_get_plugin_config():
 
 
 @django_simple_deploy.hookimpl
-def dsd_deploy():
-    """Carry out the Cloudron deployment. Implemented in platform_deployer.py."""
-    from .platform_deployer import PlatformDeployer
+def dsd_get_plugin_cli(parser):
+    """Add plugin-specific CLI args."""
+    PluginCLI(parser)
 
+
+@django_simple_deploy.hookimpl
+def dsd_validate_cli(options):
+    """Validate and parse plugin-specific CLI args."""
+    validate_cli(options)
+
+
+@django_simple_deploy.hookimpl
+def dsd_deploy():
+    """Carry out the Cloudron deployment."""
     PlatformDeployer().deploy()
